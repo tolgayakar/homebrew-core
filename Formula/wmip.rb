6175ab9ac04d35dbaa73c07267f28e6bda50e308
Tolga Yakar <tolga-yakar@hotmail.com>
2025-01-15 02:41:27 +0300

    feat: add formula for wmip

---
+++ Formula/wmip.rb
@@ -0,0 +1,18 @@
+class Wmip < Formula
+  desc "A lightweight CLI tool to fetch your public IP address"
+  homepage "https://github.com/tolgayakar/wmip"
+  url "https://github.com/tolgayakar/wmip/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "3fd3ba770ea09117368b79295bf55d53e5fd90bdb86ebf71403447452599078d"
+  license "MIT"
+
+  depends_on :macos
+
+  def install
+    system "make", "build"
+    bin.install "wmip"
+  end
+
+  test do
+    system "#{bin}/wmip", "--version"
+  end
+end
